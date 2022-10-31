778cc8bd79d312398473c885c4532bf7f5edf60b
AnyaGalkina <anna.blackbird1@gmail.com>
2022-11-01 01:02:13 +0300

    correct news

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import {productsRouter} from "./routes/products-router";
 import {addressesRouter} from "./routes/addresses-router";
+import {newsRouter} from "./routes/news-router";
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -11,6 +12,7 @@ app.use(parserMiddleware);
 
 app.use("/products", productsRouter);
 app.use("/addresses", addressesRouter);
+app.use("/news", newsRouter);
 
 app.listen(port, () => {
     console.log("listening port 3000");

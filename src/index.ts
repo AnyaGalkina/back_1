72149ae7964a77efaaa1d6d72746994580820d58
AnyaGalkina <anna.blackbird1@gmail.com>
2022-11-18 16:32:31 +0300

    add app use cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import {newsRouter} from "./routes/news-router";
 import {cakesRouter} from './routes/cakes-router';
 import {cartRouter} from './routes/cart-router';
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000
@@ -10,9 +11,11 @@ const port = process.env.PORT || 3000
 const parserMiddleware = bodyParser({});
 app.use(parserMiddleware);
 
+app.use(cors());
 app.use("/news", newsRouter);
 app.use("/products", cakesRouter);
 app.use("/cart", cartRouter);
+
 
 app.listen(port, () => {
     console.log("listening port 3000");

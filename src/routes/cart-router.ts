322ebf32632b32e4caf7b5b28092738a1c8d9211
Anna Galkina <anna.blackbird1@gmail.com>
2023-03-20 17:58:15 +0700

    Add enum with http statuses

--- src/routes/cart-router.ts
+++ src/routes/cart-router.ts
@@ -1,24 +1,24 @@
 import {Request, Response, Router} from 'express';
-import {cartRepositories} from '../repositories/cart-db-repository';
 import {validationResult} from 'express-validator';
 import {orderValidationMiddleware} from '../middlewares/order-validation-middleware';
 import {inputValidationsMiddleware} from '../middlewares/input-validations-middleware';
+import {ordersService} from '../domain/orders-service';
+import {HTTP_STATUSE} from '../enum';
 
 export const cartRouter = Router({});
-
 
 cartRouter.post('/', orderValidationMiddleware, inputValidationsMiddleware, async (req: Request, res: Response): Promise<any> => {
 
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({errors: errors.array()})
+            return res.status(HTTP_STATUSE.BAD_REQUEST_400).json({errors: errors.array()})
         }
-        const isCreated = await cartRepositories.createNewOrder(req.body);
+        const isCreated = await ordersService.createNewOrder(req.body);
         if (isCreated) {
-           res.status(200).send("order has been sent to email");
+           res.status(HTTP_STATUSE.CREATED_201).send("order has been sent to email");
 
         } else {
-            res.status(400).send('some problem with email sending');
+            res.status(HTTP_STATUSE.BAD_REQUEST_400).send('some problem with email sending');
         }
     })
 
